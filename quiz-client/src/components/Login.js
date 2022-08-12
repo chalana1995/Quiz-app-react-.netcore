57b97b83b1284887d025d60aa7df1c3868778065
chalana1995 <chalanakalpitha72@gmail.com>
2022-08-12 22:55:37 +0530

    login page done

--- quiz-client/src/components/Login.js
+++ quiz-client/src/components/Login.js
@@ -1,7 +1,35 @@
+import { Box, Button, Card, CardContent, TextField, Typography } from '@mui/material'
 import React from 'react'
+import Center from './Center'
 
 export default function Login() {
-  return (
-    <div>Login</div>
-  )
+    return (
+        <Center>
+            <Card sx={{ width: 400 }}>
+                <CardContent sx={{textAlign: 'center'}}>
+                    <Typography variant='h3' sx={{my:3}}>Quiz App</Typography>
+                    <Box sx={{
+                        '& .MuiTextField-root': {
+                            margin: 1,
+                            width: '90%'
+                        }
+                    }}>
+                        <form noValidate autoComplete='off'>
+                            <TextField
+                                label="Email"
+                                name="email"
+                                variant='outlined'
+                            />
+                            <TextField
+                                label="Name"
+                                name="name"
+                                variant='outlined'
+                            />
+                            <Button type="submit" variant='contained' size='large' sx={{ width: '90%' }}>Start</Button>
+                        </form>
+                    </Box>
+                </CardContent>
+            </Card>
+        </Center>
+    )
 }
