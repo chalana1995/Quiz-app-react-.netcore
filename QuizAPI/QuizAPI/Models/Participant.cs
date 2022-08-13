32816d7c0035beff7db210dc55de28f8fba7a27e
chalana1995 <chalanakalpitha72@gmail.com>
2022-08-14 01:24:34 +0530

    modal class created and create connection string

--- QuizAPI/QuizAPI/Models/Participant.cs
+++ QuizAPI/QuizAPI/Models/Participant.cs
@@ -1,7 +1,22 @@
-﻿namespace QuizAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuizAPI.Models
 {
     public class Participant
     {
+        [Key]
         public int PartcipantId { get; set; }
+
+        [Column(TypeName ="nvarchar(50)")]
+        public string Email { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+
+        public string Name { get; set; }
+
+        public int Score { get; set; }
+
+        public int TimeTaken { get; set; }
     }
 }
---
+++ QuizAPI/QuizAPI/Models/QuizDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace QuizAPI.Models
+{
+    public class QuizDbContext: DbContext
+    {
+        public QuizDbContext(DbContextOptions<QuizDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Question> Questions { get; set; }
+
+        public DbSet<Participant> Participant { get; set; }
+    }
+}
