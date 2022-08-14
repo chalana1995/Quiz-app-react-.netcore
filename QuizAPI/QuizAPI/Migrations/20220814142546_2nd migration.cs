f3ab333466a7464b722a2eb05289e8237d9cb407
chalana1995 <chalanakalpitha72@gmail.com>
2022-08-14 20:01:52 +0530

    bug fixing

---
+++ QuizAPI/QuizAPI/Migrations/20220814142546_2nd migration.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuizAPI.Migrations
+{
+    public partial class _2ndmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Optiona4",
+                table: "Questions",
+                newName: "Option4");
+
+            migrationBuilder.RenameColumn(
+                name: "Optiona3",
+                table: "Questions",
+                newName: "Option3");
+
+            migrationBuilder.RenameColumn(
+                name: "Optiona2",
+                table: "Questions",
+                newName: "Option2");
+
+            migrationBuilder.RenameColumn(
+                name: "Optiona1",
+                table: "Questions",
+                newName: "Option1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Option4",
+                table: "Questions",
+                newName: "Optiona4");
+
+            migrationBuilder.RenameColumn(
+                name: "Option3",
+                table: "Questions",
+                newName: "Optiona3");
+
+            migrationBuilder.RenameColumn(
+                name: "Option2",
+                table: "Questions",
+                newName: "Optiona2");
+
+            migrationBuilder.RenameColumn(
+                name: "Option1",
+                table: "Questions",
+                newName: "Optiona1");
+        }
+    }
+}
--- QuizAPI/QuizAPI/Models/Question.cs
+++ QuizAPI/QuizAPI/Models/Question.cs
@@ -15,16 +15,16 @@ namespace QuizAPI.Models
         public string?  ImageName { get; set; }
 
         [Column(TypeName ="nvarchar(50)")]
-        public string Optiona1 { get; set; }
+        public string Option1 { get; set; }
 
         [Column(TypeName ="nvarchar(50)")]
-        public string Optiona2 { get; set; }
+        public string Option2 { get; set; }
 
         [Column(TypeName ="nvarchar(50)")]
-        public string Optiona3 { get; set; }
+        public string Option3 { get; set; }
 
         [Column(TypeName ="nvarchar(50)")]
-        public string Optiona4 { get; set; }
+        public string Option4 { get; set; }
 
         public int Answer { get; set; }
 
