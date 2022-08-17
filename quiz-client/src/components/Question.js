9dd056cf7c48a0a8f4d4cce367c01ec7bcd13fa1
chalana1995 <chalanakalpitha72@gmail.com>
2022-08-17 21:46:30 +0530

    set inteveral done

--- quiz-client/src/components/Question.js
+++ quiz-client/src/components/Question.js
@@ -1,16 +1,55 @@
-import React, { useContext } from 'react'
+import { Card, CardContent, Typography, ListItemButton, List, CardHeader } from '@mui/material'
+import React, { useContext, useEffect, useState } from 'react'
+import { createAPIEndpoint, ENDPOINTS } from '../api'
+import { getFormatedTime } from '../helper'
 import useStateContext, { stateContext } from '../hooks/useStateContext'
 
 const Question = () => {
 
-    const { context, setContext } = useStateContext()
+    const [qns, setQns] = useState([])
+    const [qnIndex, setQnIndex] = useState(0)
+    const [timeTaken, setTimeTaken] = useState(0)
 
+    let timer;
 
-    console.log("context2",context);
-    // setContext({ timeTaken: 1 })
+    const startTimer = () => {
+        timer = setInterval(() => {
+            setTimeTaken(prev => prev + 1)
+        }, [1000])
+    }
 
+    useEffect(() => {
+        createAPIEndpoint(ENDPOINTS.question)
+            .fetch()
+            .then(res => {
+                setQns(res.data)
+                startTimer()
+                console.log(res.data);
+            })
+            .catch(err => { console.log(err); })
+
+        return () => { clearInterval(timer) }
+    }, [])
+
+    console.log("====qns", qns);
     return (
-        <div>Question</div>
+        qns.length != 0 ? (
+            <Card sx={{ maxWidth: 640, mx: 'auto', mt: 5 }}>
+                <CardHeader title={'Question ' + (qnIndex + 1) + ' of 5'} action={<Typography>{getFormatedTime(timeTaken)}</Typography>} />
+                <CardContent>
+                    <Typography variant='h6'>{qns[qnIndex].qnInWords}</Typography>
+                    <List>
+                        {qns[qnIndex].options.map((item, idx) =>
+                            <ListItemButton key={idx} disableRipple>
+                                <div>
+                                    <b>{String.fromCharCode(65 + idx) + " . "}</b> {item}
+                                </div>
+                            </ListItemButton>
+                        )}
+                    </List>
+                </CardContent>
+            </Card>
+        ) : null
     )
 }
 
